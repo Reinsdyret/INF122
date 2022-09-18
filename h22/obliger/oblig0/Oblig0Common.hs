c833d7417e834cfc423399500d5d8588d5de447f
reinsdyret <lars.haukland@gmail.com>
2022-09-18 18:46:13 +0200

    fixed zeroCrossings

--- h22/obliger/oblig0/Oblig0Common.hs
+++ h22/obliger/oblig0/Oblig0Common.hs
@@ -2,7 +2,7 @@ module Oblig0Common where
 
 -- Compute the average of a list of values
 average :: (Fractional a) => [a] -> a
-average list = (sum list) / (fromIntegral (length list))
+average list = sum list / fromIntegral (length list)
 
 -- A simple lowpass filter with adjustable cut-off
 lpf :: (Fractional a) => Integer -> [a] -> a
@@ -10,6 +10,7 @@ lpf n list = average (take (fromIntegral n) list)
 
 -- A simple high pass filter with adjustable cut-off
 hpf :: (Floating a) => Integer -> [a] -> a
+hpf _ [] = 0
 hpf n (x:xs) = x - lpf n (x:xs)
 
 -- Extend a finite signal with an infinite constant past
@@ -23,16 +24,17 @@ applyFilter :: (Num a, Floating a) => ([a] -> a) -> [a] -> [a]
 applyFilter fil = map fil . iterate tail . extend
 
 -- Returns if the two given values are a zeroCrossing
-zeroCrossing :: (Num a, Ord a) => a -> a -> Bool
-zeroCrossing a b = a > 0 && b <= 0 || b > 0 && a <= 0
+zeroCrossing :: (Num a, Ord a) => a -> a -> Integer
+zeroCrossing a b
+    | a > 0 && b <= 0 || b > 0 && a <= 0 = 1
+    | otherwise = 0
 
 
 -- Count the number of zero-crossings in a signal represented by a list
 zeroCrossings :: (Num a, Ord a) => [a] -> Integer
 zeroCrossings [] = 0
-zeroCrossings list
-    | zeroCrossing (head list) (head (drop 1 list)) = 1 + zeroCrossings (drop 2 list)
-    | otherwise = zeroCrossings (drop 2 list)
+zeroCrossings [x] = 0
+zeroCrossings list = zeroCrossing (head list) (list !! 1) + zeroCrossings (drop 1 list)
 
 lowPassCutoff :: Integer
 lowPassCutoff = 2
