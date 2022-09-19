5dd01b9aba64a3eb9a23d2bc06ba3944f0b3109a
reinsdyret <lars.haukland@gmail.com>
2022-09-19 12:39:23 +0200

    yes

--- h22/obliger/oblig0/Oblig0Common.hs
+++ h22/obliger/oblig0/Oblig0Common.hs
@@ -16,7 +16,8 @@ hpf n (x:xs) = x - lpf n (x:xs)
 -- Extend a finite signal with an infinite constant past
 extend ::  Num a => [a] -> [a]
 extend [] = repeat 0
-extend list = list ++ repeat (last list)
+extend [x] = repeat x
+extend (x:xs) = x : extend xs
 
 
 -- Apply a filter to a list of values
@@ -37,7 +38,7 @@ zeroCrossings [x] = 0
 zeroCrossings list = zeroCrossing (head list) (list !! 1) + zeroCrossings (drop 1 list)
 
 lowPassCutoff :: Integer
-lowPassCutoff = 2
+lowPassCutoff = 88
 highPassCutoff :: Integer
-highPassCutoff = 1
+highPassCutoff = 85
 
---
+++ h22/obliger/oblig0/Oblig0Ver1.hs
@@ -0,0 +1,8 @@
+module Main where
+
+
+-- Function that returns true if a step is detected
+isStep :: IDK
+
+-- Recursively go through input file 2 lines at a time and run the isStep function, if true then putStrLn "Step!"
+main = putStrLn "Fuck this"
