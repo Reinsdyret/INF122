a3964a9fd7d2efc326dee9b3a38bbd3ff0eeefad
Reinsdyret <lars.haukland@gmail.com>
2022-09-23 07:09:17 +0200

    finally completed

--- h22/obliger/oblig0/Oblig0Ver1.hs
+++ h22/obliger/oblig0/Oblig0Ver1.hs
@@ -7,45 +7,36 @@ import Oblig0Common
     lowPassCutoff,
     lpf,
     zeroCrossings,
-    extend
   )
+import System.IO
 
 -- Function that returns true if a step is detected
 isStep :: Double -> Double -> Bool
 isStep a b = b >= 0 && a < 0
 
-sumData :: (Double, Double, Double) -> Double
-sumData (a,b,c) = a + b + c
 
--- Recursively go through input file 2 lines at a time and run the isStep function, if true then putStrLn "Step!"
-stepDetector :: Double -> Double -> IO()
-stepDetector x y
-  | isStep x y = putStrLn "Step!"
-  | otherwise = return ()
+-- Worked with Erling Bratli on this task were we solved it together
+stepDetector :: [Double] -> IO ()
+stepDetector [] = return ()
+stepDetector (x:xs)
+-- Cheating way to see if list repeats
+  | x == head xs = return ()
+  | isStep x (head xs) = do
+    putStrLn "Step!"
+    hFlush stdout
+    stepDetector xs
+  | otherwise = stepDetector xs
 
-filterDaData :: Double -> Double
-filterDaData summedData = let
-  processedData =
-        applyFilter (hpf highPassCutoff) $
-          applyFilter (lpf lowPassCutoff) $
-            extend [summedData]
-  in head processedData
-
--- doTheLines :: [String] -> IO ()
--- doTheLines linesOfData = do
---   let filtered = filterDaData (head linesOfData)
---   let filtered2 = filterDaData (linesOfData !! 1)
---   stepDetector filtered filtered2
---   doTheLines (drop 2 linesOfData)
-
-main :: IO ()
 main = do
   -- Read user data
-  input <- getLine
-  input2 <- getLine
-  let data1 = read input :: (Double, Double, Double)
-  let data2 = read input2 :: (Double, Double, Double)
-
-  stepDetector (filterDaData $ sumData data1) (filterDaData $ sumData data2)
-  main
-  
+  input <- getContents
+  -- Process data using filters
+  let datapoints = map read (lines input) :: [(Double, Double, Double)]
+  let summedData = map (\(a, b, c) -> a + b + c) datapoints
+  let dataLength = length summedData
+  let processedData =
+        applyFilter (hpf highPassCutoff) $
+          applyFilter (lpf lowPassCutoff) $
+            summedData
+  -- Print step for each step in processedData
+  stepDetector processedData
