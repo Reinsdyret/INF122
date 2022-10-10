7f9e0389f26738c8931d53030a9c2086cde0541a
Lars Haukland <lars.haukland@gmail.com>
2022-10-10 19:54:30 +0200

    yes

--- h22/ukesoppgaver/Week41/Week41Exercise0.hs
+++ h22/ukesoppgaver/Week41/Week41Exercise0.hs
@@ -8,4 +8,15 @@ import qualified Data.Set as Set
 type Graph n = Map n (Set n)
 
 insertEdge :: (Ord n) => n -> n -> Graph n -> Graph n
-insertEdge u v = Map.insertWith Set.union u (Set.singleton v)
+insertEdge u v graph
+    | Map.member u graph && Map.member v graph = Map.insertWith Set.union u (Set.singleton v) graph
+    | Map.member u graph = do
+        let newGraph = Map.insert v Set.empty graph
+        Map.insertWith Set.union u (Set.singleton v) newGraph
+    | Map.member v graph = do
+        let newGraph = Map.insert u Set.empty graph
+        Map.insertWith Set.union u (Set.singleton v) newGraph
+    | otherwise = do
+        let newGraph = Map.insert u Set.empty graph
+        let newGraph2 = Map.insert v Set.empty newGraph
+        Map.insertWith Set.union u (Set.singleton v) newGraph2
--- h22/ukesoppgaver/Week41/Week41Exercise1.hs
+++ h22/ukesoppgaver/Week41/Week41Exercise1.hs
@@ -5,13 +5,23 @@ import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 
+import Data.Maybe ( isNothing, fromMaybe )
+
 type Graph n = Map n (Set n)
 
 
 disjoint :: (Ord a) => Set a -> Set a -> Bool
 disjoint a b = Set.null $ Set.intersection a b
 
-hasCycle' :: (Ord n) => Graph n -> Set n -> Bool
-hasCycle' graph searched = False
+hasCycle' :: (Ord n) => Graph n -> Set n -> n -> Bool
+hasCycle' graph searched currentNode
+    | isNothing (Map.lookup currentNode graph) = False
+    | otherwise = do
+        let Just connected = Map.lookup currentNode graph
+        if disjoint searched connected then or (map (hasCycle' graph (Set.union searched (Set.singleton currentNode))) (Set.toList (fromMaybe Set.empty (Map.lookup currentNode graph))))
+        else True
+
 hasCycle :: (Ord n) => Graph n -> n -> Bool
-hasCycle graph node = False
+hasCycle graph node = do
+    let Just currentNode = Map.lookup node graph
+    hasCycle' graph currentNode currentNode
