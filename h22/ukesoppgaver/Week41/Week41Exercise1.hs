846923d29285cb694500c06be1ec45cc45f5b428
Reinsdyret <lars.haukland@gmail.com>
2022-10-10 21:36:56 +0200

    finally done

--- h22/ukesoppgaver/Week41/Week41Exercise1.hs
+++ h22/ukesoppgaver/Week41/Week41Exercise1.hs
@@ -5,7 +5,7 @@ import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 
-import Data.Maybe ( isNothing, fromMaybe )
+import Data.Maybe ( isNothing, fromJust )
 
 type Graph n = Map n (Set n)
 
@@ -14,14 +14,13 @@ disjoint :: (Ord a) => Set a -> Set a -> Bool
 disjoint a b = Set.null $ Set.intersection a b
 
 hasCycle' :: (Ord n) => Graph n -> Set n -> n -> Bool
-hasCycle' graph searched currentNode
+hasCycle' graph visited currentNode
     | isNothing (Map.lookup currentNode graph) = False
     | otherwise = do
-        let Just connected = Map.lookup currentNode graph
-        if disjoint searched connected then or (map (hasCycle' graph (Set.union searched (Set.singleton currentNode))) (Set.toList (fromMaybe Set.empty (Map.lookup currentNode graph))))
-        else True
+        let newVisited = Set.insert currentNode visited
+        let children = fromJust (Map.lookup currentNode graph)
+        not (Set.null children) && not (disjoint newVisited children) || any (hasCycle' graph newVisited) (Set.toList children)
+
 
 hasCycle :: (Ord n) => Graph n -> n -> Bool
-hasCycle graph node = do
-    let Just currentNode = Map.lookup node graph
-    hasCycle' graph currentNode currentNode
+hasCycle graph = hasCycle' graph Set.empty
