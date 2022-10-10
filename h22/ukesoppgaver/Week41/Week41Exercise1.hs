25a8e7a59bf2363fcbf12a6c73395c30edeae262
Lars Haukland <lars.haukland@gmail.com>
2022-10-10 12:15:04 +0200

    yes

--- h22/ukesoppgaver/Week41/Week41Exercise1.hs
+++ h22/ukesoppgaver/Week41/Week41Exercise1.hs
@@ -11,7 +11,7 @@ type Graph n = Map n (Set n)
 disjoint :: (Ord a) => Set a -> Set a -> Bool
 disjoint a b = Set.null $ Set.intersection a b
 
---hasCycle' :: (Ord n) => Graph n -> 
-
+hasCycle' :: (Ord n) => Graph n -> Set n -> Bool
+hasCycle' graph searched = False
 hasCycle :: (Ord n) => Graph n -> n -> Bool
 hasCycle graph node = False
