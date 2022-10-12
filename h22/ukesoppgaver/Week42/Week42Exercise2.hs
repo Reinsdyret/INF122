8f5f9dd8bada180ad409b0df284feecc0085b4dd
Lars Haukland <lars.haukland@gmail.com>
2022-10-12 14:51:07 +0200

    yes

--- h22/ukesoppgaver/Week42/Week42Exercise2.hs
+++ h22/ukesoppgaver/Week42/Week42Exercise2.hs
@@ -4,5 +4,6 @@ module Week42Exercise2 where
 reverseWord :: String -> String
 reverseWord = unwords . map reverse . words
 
+
 sumIsEven :: Integer -> Integer -> Bool
-sumIsEven = \a b -> even $ a+b
+sumIsEven = (.) even . (+)
