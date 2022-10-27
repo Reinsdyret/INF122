84c2060173aa8eea23ee88970f2be5a3f4de7f49
Lars Haukland <lars.haukland@gmail.com>
2022-10-27 19:58:15 +0200

    soon done

--- h22/ukesoppgaver/Week43/Week43Exercise2.hs
+++ h22/ukesoppgaver/Week43/Week43Exercise2.hs
@@ -8,10 +8,22 @@ class IntegerGraph g where
   nodeInGraph :: Integer -> g -> Bool
   edgeInGraph :: Integer -> Integer -> g -> Bool
 
-type MyGraph g = IntegerGraph g
-  deriving (Show)
+data MyGraph = MyGraph [Integer] [(Integer, Integer)]
+  deriving (Show, Eq)
 
+instance IntegerGraph MyGraph where
+  emptyGraph = MyGraph [] []
+  insertNode u (MyGraph nodes edges) = MyGraph (u : nodes) edges
+  insertEdge u v (MyGraph nodes edges) = MyGraph nodes ((u,v) : edges)
+  nodeInGraph u (MyGraph nodes edges) = elem u nodes
+  edgeInGraph u v (MyGraph nodes edges) = elem (u,v) edges
 
+graph :: (IntegerGraph g) => g
+graph =
+  let graphWithNodes = insertNode 8 
+    (insert 6 
+    (insert 5 
+    (insert 3
+    (insert 1 emptyGraph))))
 
-
-
+ in insertEdge 1 6 (insertEdge 1 8 (insertEdge 5 1 (insertEdge 5 8 (insertEdge 8 5 graphWithNodes))))
