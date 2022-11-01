9e2e3c4d3492042898e80c338aabb2a55a9501af
Lars Haukland <lars.haukland@gmail.com>
2022-11-01 16:36:22 +0100

    done 1a

--- h22/obliger/oblig1/Model.hs
+++ h22/obliger/oblig1/Model.hs
@@ -19,13 +19,19 @@ emptyModel = Map.empty
 
 -- Update a model with a new n-gram followed by a character.
 increaseWeight :: NGram -> Char -> TextModel -> TextModel
-increaseWeight ngram next = _
+increaseWeight ngram next model
+  | not (Map.member ngram model) = Map.insert ngram (Map.fromList [(next, 1)], 1) model 
+  | otherwise = do
+    let Just (charModel, weight) = Map.lookup ngram model
+    let newNgramModel = Map.insertWith (+) next 1 charModel
+    let newWeight = weight + 1
+    Map.insert ngram (newNgramModel, newWeight) model
 
 -- The distribution of next n-grams after a given one.
 nextDistribution :: TextModel -> NGram -> Maybe ([(NGram, Weight)],Weight)
-nextDistribution model current = _
+nextDistribution model current = Nothing
 
 -- Create an n-gram model from a string.
 createModel :: Integer -> String -> TextModel
-createModel n = _
+createModel n str = emptyModel
 
